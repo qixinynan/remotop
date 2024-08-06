9c075beeddfea6d0dedb8cc322b1e4a478476ca5
Qixinies <3185835784@qq.com>
2024-08-06 21:18:39 +0800

    feat: new pattle

--- main.cpp
+++ main.cpp
@@ -87,13 +87,26 @@ public:
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     QApplication::setStyle(QStyleFactory::create("Fusion"));
-
-    QPalette palette;
-    palette.setColor(QPalette::Window, Qt::white);
-    palette.setColor(QPalette::WindowText, Qt::black);
-    app.setPalette(palette);
-
     SettingsWindow window;
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    app.setPalette(darkPalette);
+
     window.setWindowTitle("Remotop");
     window.resize(600, 400);
     window.show();
